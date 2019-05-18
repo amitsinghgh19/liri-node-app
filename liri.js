a606e9493da6a573810719a10130b051ce06cc5c
amitsinghgh19 <prasadmanigh19>
2019-05-17 23:39:43 -0400

    added catch error in getOMDB function

--- liri.js
+++ liri.js
@@ -197,7 +197,26 @@ function getOMDB(movie) {
                     console.log("response status is "+response.status);
                 }
                 
-            });
+            }).catch(function(error) {
+                if (error.response) {
+                  // The request was made and the server responded with a status code
+                  // that falls out of the range of 2xx
+                  console.log("---------------Data---------------");
+                  console.log(error.response.data);
+                  console.log("---------------Status---------------");
+                  console.log(error.response.status);
+                  console.log("---------------Status---------------");
+                  console.log(error.response.headers);
+                } else if (error.request) {
+                  // The request was made but no response was received
+                  // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                  console.log(error.request);
+                } else {
+                  // Something happened in setting up the request that triggered an Error
+                  console.log("Error:", error.message);
+                }
+                console.log(error.config);
+              });
         };
 
 // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
