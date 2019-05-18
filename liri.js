5d00dcf2c509fbd87090dc564d44493e040e5186
amitsinghgh19 <prasadmanigh19>
2019-05-17 23:46:50 -0400

    added catch error getBandsInTown function

--- liri.js
+++ liri.js
@@ -126,7 +126,26 @@ function getBandsInTown(artist) {
                 console.log("response status is "+response.status);
             }
                 
-            });
+            }).catch(function(error) {
+                if (error.response) {
+                  // The request was made and the server responded with a status code
+                  // that falls out of the range of 2xx
+                  console.log("---------------Data---------------");
+                  console.log(error.response.data);
+                  console.log("---------------Status---------------");
+                  console.log(error.response.status);
+                  console.log("---------------Status---------------");
+                  console.log(error.response.headers);
+                } else if (error.request) {
+                  // The request was made but no response was received
+                  // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                  console.log(error.request);
+                } else {
+                  // Something happened in setting up the request that triggered an Error
+                  console.log("Error", error.message);
+                }
+                console.log(error.config);
+              });
         };
 
 //Function to search Movie Info using OMDB API
@@ -234,7 +253,8 @@ function getRandom() {
                 }
                 //console.log("\r\n" + "testing: " + randomData[0] + randomData[1]);
 
-            });
+            })
+            ;
         };
 
 //Execute function
